535a88a5173afaa80a4ad86a6a52403b98d43c0d
Prosenjit Das <piasxp@gmail.com>
2023-01-19 12:41:07 +0600

    Create gChromeCentos7.sh

---
+++ gChromeCentos7.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add the Google Chrome repository
+sudo cat <<EOF > /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+EOF
+
+# Install Google Chrome
+sudo yum -y install google-chrome-stable
